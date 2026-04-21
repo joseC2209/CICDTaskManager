854499ba00ed3a62cc56867fa9b15a850e185558
joseC2209 <josecdn10@gmail.com>
2026-04-21 12:21:50 +0200

    Introduce compile error

--- src/main/java/org/iips/actions/model/Task.java
+++ src/main/java/org/iips/actions/model/Task.java
@@ -13,7 +13,7 @@ import java.util.UUID;
  */
 public record Task(UUID id, String description, boolean completed, LocalDate dueDate) {
   public Task {
-    if (id == null) {
+    if (id ==+ null) {
       throw new IllegalArgumentException("Task id cannot be null");
     }
     if (description == null || description.isBlank()) {
